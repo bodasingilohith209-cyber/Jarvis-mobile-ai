f24aa920938a774cd2ef1d2c1e204d293aedc8c9
bodasingilohith209-cyber <bodasingilohith209@gmail.com>
2026-09-22 10:00:31 +0530

    Update Chat.js

--- api/Chat.js
+++ api/Chat.js
@@ -1,1 +1,27 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      error: "Only POST requests are allowed"
+    });
+  }
 
+  try {
+    const { message } = req.body;
+
+    if (!message) {
+      return res.status(400).json({
+        error: "Message is required"
+      });
+    }
+
+    // AI connection will be added here next.
+    return res.status(200).json({
+      reply: "J.A.R.V.I.S received: " + message
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      error: "J.A.R.V.I.S brain error"
+    });
+  }
+}
